f5bdd925c276581729172afe37328156d74bde4c
Samuel Roberts <rober352@purdue.edu>
2017-05-23 16:32:53 -0400

    Make Things mucho pretty

---
+++ src/Thing.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Thing.css'
+
+const Thing = ({thing}) => {
+    return (
+        <li className="Thing">
+          <input type="checkbox" value="on" />
+          <div className="details">
+            <div className="name">
+              {thing.name}
+            </div>
+            <span className="actions">
+              <button className="remove">
+                <i className="fa fa-trash-o"></i>
+              </button>
+            </span>
+          </div>
+        </li>
+    )
+}
+
+export default Thing
